42ae6eafd28f87118be9d768242b09b0f3b0a015
nsmoliak <natallia.smoliak@intexsoft.by>
2023-04-16 00:43:41 +0300

    add docker-compose with postgresql + spring app templtate and simple db integration

---
+++ dream-x-testapp/README.md
@@ -0,0 +1,15 @@
+# Dream-x Test Application
+
+## System requirements
+- Java 11
+- Docker 19+
+- Docker Compose
+- Postgresql 15
+
+## Dockerizing the apps
+
+### All apps via docker-compose
+
+1. `cd docker`
+2. `docker-compose -f docker-compose.yml --env-file .env.dev up -d`
+
---
+++ dream-x-testapp/src/main/java/com/dreamx/testproject/DreamXTestApplication.java
@@ -0,0 +1,11 @@
+package com.dreamx.testproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamXTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DreamXTestApplication.class, args);
+    }
+}
